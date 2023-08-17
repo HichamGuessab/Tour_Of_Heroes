54805fc84ae4d975f3cd09da5ac5873ed39157f8
HichamGsb <hichamgsb.pro@gmail.com>
2023-08-17 14:29:02 +0200

    Navigating to hero details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 // This tells the router to match that URL to path: 'heroes' and display the HeroesComponent when the URL is something like localhost:4200/heroes.
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },   // The colon ':' character in the path indicates that :id is a placeholder for a specific hero id.
   { path: 'dashboard', component: DashboardComponent }
 ];
 
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,4 +18,15 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes')
     return heroes;
   }
+
+
+  // Like getHeroes(), getHero() has an asynchronous signature. 
+  // It returns a mock hero as an Observable, using the RxJS of() function.
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
