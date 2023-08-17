2e311918b8a1c0a16716f606741df37944e44960
HichamGsb <hichamgsb.pro@gmail.com>
2023-08-17 16:22:48 +0200

    Bug : the data changed on the app doesn't save on the server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,7 +15,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes'; // URL to web API
+  private heroesUrl = '4200/heroes'; // URL to web API
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
@@ -62,4 +65,12 @@ export class HeroService {
       return of(result as T);
     };
   }
+
+    /** PUT: update the hero on the server */
+    updateHero(hero: Hero): Observable<any> {
+      return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+    }
 }
